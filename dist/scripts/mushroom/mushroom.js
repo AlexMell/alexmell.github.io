695f8d88d59f216593ee1850a003858e4e29bd0c
AlexMell <alex2mell@gmail.com>
2017-07-03 22:15:47 +0300

    Star mushroom canvas

---
+++ dist/scripts/mushroom/mushroom.js
@@ -0,0 +1,25 @@
+// Mushroom canvas
+
+'use strict';
+
+function mushroom() {
+    // Create canvas
+    var canvas = document.getElementById('mushroom');
+    var context = canvas.getContext('2d');
+
+    // Make canvas full width
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Create gradient rectangle full width
+    context.beginPath();
+    var grd = context.createLinearGradient(0, canvas.height, canvas.width, 0);
+
+    grd.addColorStop(0, "#3b0539");
+    grd.addColorStop(0.5, "#090854");
+    grd.addColorStop(1, "#491792");
+    context.fillStyle = grd;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.closePath();
+}
+mushroom();
