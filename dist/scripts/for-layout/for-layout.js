4af6fc0c86f53b7ca9e47953fe745b43d6130f05
AlexMell <alex2mell@gmail.com>
2018-01-01 22:54:51 +0200

    Add follow typing template

--- dist/scripts/for-layout/for-layout.js
+++ dist/scripts/for-layout/for-layout.js
@@ -4,3 +4,21 @@ $('.js-accordion-button').on('click', function () {
     $(this).find('.js-accordion-descr').toggleClass('is-active');
     $(this).siblings().find('.js-accordion-descr').removeClass('is-active');
 });
+
+function followType() {
+
+    var $head = $('.js-bear-head');
+    var $input = $('.js-follow-input');
+
+    $input.on('keyup keypress change blur keydown', function () {
+
+        var maxLength = $input.attr('maxlength');
+
+        var value = $(this).val();
+        var valueLength = value.length;
+        var setRotate = maxLength - valueLength * 2.2;
+
+        valueLength >= maxLength ? false : $head.css('transform', 'rotate(' + setRotate + 'deg)');
+    });
+}
+followType();
--- src/scripts/for-layout/for-layout.js
+++ src/scripts/for-layout/for-layout.js
@@ -2,3 +2,22 @@ $('.js-accordion-button').on('click', function () {
     $(this).find('.js-accordion-descr').toggleClass('is-active');
     $(this).siblings().find('.js-accordion-descr').removeClass('is-active');
 });
+
+function followType () {
+
+    let $head = $('.js-bear-head');
+    let $input = $('.js-follow-input');
+
+    $input.on('keyup keypress change blur keydown', function () {
+
+        const maxLength = $input.attr('maxlength');
+
+        let value = $(this).val();
+        let valueLength = value.length;
+        let setRotate = maxLength - valueLength*2.2;
+
+        valueLength >= maxLength ? false : $head.css('transform','rotate('+ setRotate +'deg)');
+
+    });
+}
+followType();
