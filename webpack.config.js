a4526a940ceeba7aa2b210276e9715c1d3129016
AlexMell <alex2mell@gmail.com>
2018-01-03 21:08:32 +0200

    Webpack start

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,12 @@
 'use strict';
+
+var path = require('path');
+
+module.exports = {
+    entry: './home.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'build.js',
+        library: "home"
+    }
+}
