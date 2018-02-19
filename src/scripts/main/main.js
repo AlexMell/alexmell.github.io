96449a1f2ef185cd429c4dfe98797d499be53701
AlexMell <alex2mell@gmail.com>
2018-02-19 23:23:59 +0200

    Start learning react.js

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -1,11 +1,9 @@
-var test = new TimelineMax();
+var anim = new TimelineMax();
 
 window.onload = function() {
-
-    test
+    anim
         .fromTo('.header__author', 1, {y: -200, opacity: 0}, {y: 0, opacity: 1})
         .fromTo('.soc-net__mail-link', 1, {y: 10, opacity: 0}, {y: 0, opacity: 1}, 0.2)
         .staggerFromTo('.soc-net__item', 0.5, {opacity: 0, y: 10}, {opacity: 1, y:0}, 0.1)
         .staggerFromTo('.pages__division', 0.5, {opacity: 0, x: -200}, {opacity: 1, x: 0}, 0.1);
-
 };
