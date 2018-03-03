34ed99f9fb43896735b83e8d3bbea93f7354a30a
AlexMell <alex2mell@gmail.com>
2018-03-03 17:43:55 +0200

    Fix main page loading

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -1,9 +1,20 @@
-var anim = new TimelineMax();
 
-window.onload = function() {
+$(window).on("load", function () {
+
+    var anim = new TimelineMax();
+
+    
+    $(document).ready(function () {
+
+        var body = $('body');
+        body.removeClass('page-load');
+        
+    });
+
     anim
-        .fromTo('.header__author', 1, {y: -200, opacity: 0}, {y: 0, opacity: 1})
-        .fromTo('.soc-net__mail-link', 1, {y: 10, opacity: 0}, {y: 0, opacity: 1}, 0.2)
-        .staggerFromTo('.soc-net__item', 0.5, {opacity: 0, y: 10}, {opacity: 1, y:0}, 0.1)
-        .staggerFromTo('.pages__division', 0.5, {opacity: 0, x: -200}, {opacity: 1, x: 0}, 0.1);
-};
+        .fromTo('.main-page', 0.5, {opacity: 0}, {opacity: 1})
+        .fromTo('.header__author', 0.5, {y: -200, opacity: 0}, {y: 0, opacity: 1})
+        .fromTo('.soc-net__mail-link', 0.5, {y: 10, opacity: 0}, {y: 0, opacity: 1}, 0.1)
+        .staggerFromTo('.soc-net__item', 0.2, {opacity: 0, y: 10}, {opacity: 1, y:0}, 0.1)
+        .staggerFromTo('.pages__division', 0.5, {opacity: 0, x: -100}, {opacity: 1, x: 0}, 0.1);
+})
